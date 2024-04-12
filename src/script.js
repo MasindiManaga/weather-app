b5a9b7bc8b0838f8c675f8f5ac23f06a0e614bed
MasindiManaga <134053229+MasindiManaga@users.noreply.github.com>
2024-04-13 01:39:33 +0200

    added Api

--- src/script.js
+++ src/script.js
@@ -1,8 +1,27 @@
+function updateWeather(response){
+    let temperatureElement = document.querySelector("#weather-temperature");
+    let temperature = response.data.temperature.current;
+let cityElement = document.querySelector("#weather-app-city");
+cityElement.innerHTML = inputText.value;
+
+cityElement = response.data.city;
+temperatureElement.innerHTML = Math.round(temperature);
+ 
+}
+
+
+function searchCity(city){
+let apiKey = "o2719tc03b80c7441be54fbaafcfc88f";
+let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiURL).then(updateWeather);
+}
+
+
 function handleSearchSubmit(event){
 event.preventDefault();
 let inputText = document.querySelector("#input-text");
-let cityElement = document.querySelector("#weather-app-city");
-cityElement.innerHTML = inputText.value;
+
+searchCity(inputText.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
