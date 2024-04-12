d03496621fbacf5dfc3ff346fdeac3728760f463
MasindiManaga <134053229+MasindiManaga@users.noreply.github.com>
2024-04-13 01:17:00 +0200

    Updates to html and js
    added id's and classes to html
    In js I had to get the focus on the search input section

--- src/script.js
+++ src/script.js
@@ -1,2 +1,9 @@
+function handleSearchSubmit(event){
+event.preventDefault();
+let inputText = document.querySelector("#input-text");
+let cityElement = document.querySelector("#weather-app-city");
+cityElement.innerHTML = inputText.value;
+}
+
 let searchFormElement = document.querySelector("#search-form");
-console.log(searchFormElement);
+searchFormElement.addEventListener("submit", handleSearchSubmit);
